e34e09eccd6016ca93f6a773f1a32088903fdc8a
Andros Fenollosa <andros@fenollosa.email>
2021-07-02 21:29:55 +0200

    Add sort

--- src/rsspaper/core.clj
+++ src/rsspaper/core.clj
@@ -1,10 +1,10 @@
 (ns rsspaper.core
   (:require
-   [rsspaper.feeds :refer [get-feeds]]
+   [rsspaper.feeds :refer [get-articles]]
    [rsspaper.html :refer [make-html]]) (:gen-class))
 
 (defn -main [& args]
   ;; Main
   (prn "Reading feeds... Please be patient.")
-  (make-html (get-feeds))
+  (make-html (get-articles))
   (prn "Generated \uD83D\uDCF0 in 'dist/index.html'!"))
--- src/rsspaper/feeds.clj
+++ src/rsspaper/feeds.clj
@@ -8,32 +8,44 @@
 (def date-custom-formatter (f/formatter "dd MM yyyy"))
 
 (defn datetimes-to-unixtime
-  [data]
-  (map (fn [blog]
-         (assoc-in blog [:feed :entries]
-                   (map (fn [article]
-                            (assoc article :published-date (c/to-long (:published-date article))))
-                        (get-in blog [:feed :entries])))) data))
+  [articles]
+  (map (fn [article]
+         (assoc article :published-date (c/to-long (:published-date article)))) articles))
 
 
 (defn add-datetimes-formatter
-  [data]
-  (map (fn [blog]
-         (assoc-in blog [:feed :entries]
-                   (map (fn [article]
-                          (assoc article :published-date-formatter (f/unparse date-custom-formatter (c/from-long (:published-date article)))))
-                   (get-in blog [:feed :entries])))) data))
+  [articles]
+  (map (fn [article]
+         (assoc article :published-date-formatter (f/unparse date-custom-formatter (c/from-long (:published-date article))))) articles))
 
 
-(defn get-feeds
+(defn zip-feeds-in-articles
+  [feeds]
+  ;; Flat all articles
+  (reduce (fn [articles feed]
+            ;; Add in every article, all information from feed
+             (concat articles (map (fn [article] (assoc article :feed (:feed (update-in feed [:feed] dissoc :entries)) )) (get-in feed [:feed :entries])))) [] feeds))
+
+
+(defn add-cover-article
+  [articles]
+  ;; Add cover to article search first image in description
+  ;; Iterate every blog
+  (map (fn [article]
+         (assoc article :cover (second (re-find #"<img[^>]+src=\"([^\">]+)\"" (str  (get-in article [:description :value])))))
+         ) articles))
+
+
+(defn get-articles
   []
   ;; Get all feeds from config -> feeds
-  (->
+  (reverse (sort-by :published-date (->
      (reduce
        (fn [feeds feed-url]
          (conj feeds
                (parse-url feed-url {:insecure? true :throw-exceptions false}))
          ) [] (:feeds config))
+     zip-feeds-in-articles
+     add-cover-article
      datetimes-to-unixtime
-     add-datetimes-formatter))
-
+     add-datetimes-formatter))))
